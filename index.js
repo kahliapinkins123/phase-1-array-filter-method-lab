7bfcdb4ec7b6f6f950d958d4fd657b3871a3336c
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-09-09 21:18:40 -0400

    commit tree 7ddd51631f2c47d1f1e2e411c4feacfe14946cdd

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(array, string){
+
+    let result = array.filter(function(str){
+        let isEqual = str.toUpperCase() === string.toUpperCase();
+        return isEqual;
+    })
+
+   return result;
+}
+
+function fuzzyMatch(array,string){
+    let result = array.filter(function(str){
+        let isEqual = str.charAt(0).toUpperCase() === string.charAt(0).toUpperCase();
+        return isEqual;
+    })
+
+   return result;
+}
+
+function matchName(array,string){
+    let result = array.filter(function(str){
+        let isEqual = str.name === string;
+        return isEqual;
+    })
+
+   return result;
+}
